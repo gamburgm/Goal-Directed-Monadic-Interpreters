75ddaea4f5f3f7355fb99f0d79fc70eca0a68ff1
gamburgm <mitch.gamburg@gmail.com>
2020-10-31 13:18:42 -0400

    changes

--- src/kont2_interp.ts
+++ src/kont2_interp.ts
@@ -6,10 +6,21 @@ export function interpret(prog: Program) {
   if (prog.kind === 'Int') {
     return mops.unit(prog.val);
   } else if (prog.kind === 'To') {
-    
+    return (k: N2Kont) => {
+      return interpret(prog.from)((i: number) => interpret(prog.to)((j: number) => to(i)(j)(k)));
+    }
   } else if (prog.kind === 'Add') {
+    return (k: N2Kont) => {
+      return interpret(prog.first)((i: number) => interpret(prog.second)((j: number) => k(i + j)));
+    }
   } else if (prog.kind === 'LT') {
+    return (k: N2Kont) => {
+      return interpret(prog.lte)((i: number) => interpret(prog.gte)((j: number) => leq(i)(j)(k)));
+    }
   } else if (prog.kind === 'If') {
+    return (k: N2Kont) => (f: NStream) => {
+      return interpret(prog.cond)((_: any) => (_: any) => interpret(prog.yes)(k)(f))(() => interpret(prog.no)(k)(f));
+    }
   }
 }
 
